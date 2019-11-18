8cf3f894b8267bceaabfb38a75a75291dd699b65
rama10 <ranantatmula@unomaha.edu>
2019-11-17 23:23:07 -0700

    without the venv

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\vinsai01\Python\F19-4900-2\Assignments1-2-3\Assign2 B\mfscrm\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

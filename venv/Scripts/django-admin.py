de3220f6f2ef261e7d2b52faaaacd763bc531af0
rama10 <ranantatmula@unomaha.edu>
2019-11-17 21:45:41 -0700

    commiting files to heroku

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\vinsai01\Python\F19-4900-2\Assignments1-2-3\Assign2 B\mfscrm\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
